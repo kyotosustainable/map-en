55f2eeb04ef938a4cf66afc2141e2fad30e33a1c
airi <airi.hisa@icloud.com>
2026-09-07 23:06:25 +0900

    Update ShopListItem.tsx

--- src/App/ShopListItem.tsx
+++ src/App/ShopListItem.tsx
@@ -9,12 +9,16 @@ type Props = {
 const ShopListItem = (props: Props) => {
   const { data, popupHandler } = props;
 
+  // 英語ヘッダー（Spot Name）を優先し、フォールバックとして旧名も参照
+  const title = data['Spot Name'] || data['スポット名'] || '';
+  const category = data['Category'] || data['カテゴリ'] || '';
+
   return (
     <div className="shop-list-item" onClick={() => popupHandler(data)}>
       {/* 左側：正方形の画像エリア */}
       <div className="item-image-container">
         {data['画像'] ? (
-          <img src={data['画像']} alt={data['スポット名']} />
+          <img src={data['画像']} alt={title} />
         ) : (
           <div className="no-image">No Image</div>
         )}
@@ -22,8 +26,8 @@ const ShopListItem = (props: Props) => {
 
       {/* 右側：コンテンツエリア */}
       <div className="item-content">
-        <div className="item-category-tag">{data['カテゴリ']}</div>
-        <h3 className="item-title">{data['スポット名']}</h3>
+        <div className="item-category-tag">{category}</div>
+        <h3 className="item-title">{title}</h3>
         {/* スプレッドシートに日付列などがあればここに追加できます */}
         <p className="item-date">{data['タイムスタンプ']?.split(' ')[0]}</p>
       </div>
